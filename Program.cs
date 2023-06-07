01ac0e72a1ded8991fb748626932d1144ed1c91c
Brahma <swlw.brahma@dell.com>
2023-06-07 10:06:09 +0530

    Init commit

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        void Main(string[] args)
+        {
+            Console.WriteLine("Hello viewer, Welcome to .Net FSD program !");
+            //Console.ReadLine();
+
+            //int a = 123456789;
+            //long b = a;
+            //Console.WriteLine(b);
+
+
+            Console.WriteLine("Arithmetic Calculator");
+
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Select an arithmetic operation:");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            Console.Write("Enter your choice (1-4): ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            double result = 0;
+
+            switch (choice)
+            {
+                case 1:
+                    result = num1 + num2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+                case 2:
+                    result = num1 - num2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+                case 3:
+                    result = num1 * num2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+                case 4:
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine("Result: " + result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Cannot divide by zero");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program_01.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program_01
+    {
+        static void Main(string[] args)
+        {
+            DateTime d11 = new DateTime(2023, 06, 7);
+            Console.WriteLine(d11);
+
+            DateTime d22 = new DateTime(2023, 06, 7, 10, 24, 35);
+            Console.WriteLine(d22);
+            DateTime today = DateTime.Now;
+            Console.WriteLine(today);
+
+            DateTime d3 = today.AddDays(10);
+            Console.WriteLine(d3);
+
+            DateTime d1 = new DateTime(2023, 01, 07);
+            DateTime d2 = new DateTime(2023, 01, 08);
+
+            TimeSpan diff = d2 - d1;
+            Console.WriteLine(diff);
+
+            TimeSpan diff1 = d2.Subtract(d1);
+            Console.WriteLine(diff1);
+
+            Console.ReadLine();
+        }
+    }
+}
